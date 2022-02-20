80b89be610b2127449223ee3235c3f5ee1b6549b
Paweł Prorok <pawel3474@tlen.pl>
2022-02-20 12:16:10 +0100

    Added logic to burger button

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 const navMobile = document.querySelector('.nav-mobile')
+const nav = document.querySelectorAll('.nav__link')
 const navBtn = document.querySelector('.hamburger')
 const footerYear = document.querySelector('.footer__year')
 
 const handleNav = () => {
     navBtn.classList.toggle('is-active')
     navMobile.classList.toggle('nav-mobile--active')
+
+    nav.forEach(item => {
+        item.addEventListener('click', () => {
+            navMobile.classList.remove('nav-mobile--active')
+        })
+    })
 }
 
 navBtn.addEventListener('click', handleNav)
